0c12047cf48754ce291c70104d9ebf4ba43a5ca3
Richa <66673366+Richa139@users.noreply.github.com>
2022-02-11 18:41:16 +0530

    Add files via upload

---
+++ WeatherAppNodejs/index.js
@@ -0,0 +1,37 @@
+const http = require("http");
+const fs= require("fs");
+var requests= require("requests");
+const homefile = fs.readFileSync('home.html',"utf-8");
+
+const replaceval=(tempval,orgval)=>{
+ let temperature=tempval.replace("{%tempval%}",orgval.main.temp);
+ temperature = temperature.replace("{%tempmin%}", orgval.main.temp_min);
+  temperature = temperature.replace("{%tempmax%}", orgval.main.temp_max);
+  temperature = temperature.replace("{%city%}", orgval.name);
+  temperature = temperature.replace("{%country%}", orgval.sys.country);
+  // temperature = temperature.replace("{%tempstatus%}", orgval.weather[0].main);
+};
+const server=http.createServer((req,res)=>{
+  if(req.url=="/"){
+    requests("https://api.openweathermap.org/data/2.5/weather?q=Pune&appid=d2a67dcf670c442225c5149c041b5ce7")
+.on('data',  (chunk)=> {
+    const objdata=JSON.parse(chunk);
+    const arraydata=[objdata];
+  // console.log(array[0].main.temp);
+
+  const realdata=arraydata.map((val)=>replaceval(homefile,val))
+  .join("");
+  res.write(realdata);
+  console.log(realdata);
+})
+.on('end',  (err) =>{
+  if (err) return console.log('connection closed due to errors', err);
+  res.end();
+});
+  }
+  else{
+    res.end("File not found");
+  }
+});
+
+server.listen(3000,"127.0.0.1");
