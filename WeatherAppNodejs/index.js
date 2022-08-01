7d27933a5b3c3a9baacfb8fadcd911f83ac019a3
Richa Kochar <66673366+Richa139@users.noreply.github.com>
2022-08-01 15:22:02 +0530

    Update index.js

--- WeatherAppNodejs/index.js
+++ WeatherAppNodejs/index.js
@@ -1,27 +1,32 @@
 const http = require("http");
 const fs= require("fs");
+const path=require("path");
 var requests= require("requests");
-const homefile = fs.readFileSync('home.html',"utf-8");
-
+const homefile = fs.readFileSync("home.html","utf-8");
 const replaceval=(tempval,orgval)=>{
  let temperature=tempval.replace("{%tempval%}",orgval.main.temp);
  temperature = temperature.replace("{%tempmin%}", orgval.main.temp_min);
   temperature = temperature.replace("{%tempmax%}", orgval.main.temp_max);
   temperature = temperature.replace("{%city%}", orgval.name);
   temperature = temperature.replace("{%country%}", orgval.sys.country);
-  // temperature = temperature.replace("{%tempstatus%}", orgval.weather[0].main);
+  temperature = temperature.replace("{%tempstatus%}", orgval.weather[0].main);
+  return temperature;
 };
 const server=http.createServer((req,res)=>{
-  if(req.url=="/"){
+  if(req.url=="/weather"){
     requests("https://api.openweathermap.org/data/2.5/weather?q=Pune&appid=d2a67dcf670c442225c5149c041b5ce7")
 .on('data',  (chunk)=> {
     const objdata=JSON.parse(chunk);
     const arraydata=[objdata];
-  // console.log(array[0].main.temp);
-
-  const realdata=arraydata.map((val)=>replaceval(homefile,val))
+  console.log(arraydata[0].main.temp);
+  // res.write(homefile)
+  const realdata=arraydata.map(function (val) {
+    return replaceval(homefile, val);
+  })
   .join("");
   res.write(realdata);
+  // res.sendFile('home.html');
+  
   console.log(realdata);
 })
 .on('end',  (err) =>{
@@ -34,4 +39,6 @@ const server=http.createServer((req,res)=>{
   }
 });
 
-server.listen(3000,"127.0.0.1");
+server.listen(7001 ,"127.0.0.1",function(){
+  console.log("server is listening");
+})
